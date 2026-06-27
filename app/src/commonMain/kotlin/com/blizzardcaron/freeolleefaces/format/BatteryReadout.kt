6c469862ab63ce3459c97e228daca06b2e247313
Ken Blizzard-Caron <ken@blizzardcaron.com>
2026-06-27 10:29:29 -0600

    feat(battery): add BatteryReadout volts/percent enum

---
+++ app/src/commonMain/kotlin/com/blizzardcaron/freeolleefaces/format/BatteryReadout.kt
@@ -0,0 +1,4 @@
+package com.blizzardcaron.freeolleefaces.format
+
+/** How the Battery complication renders the watch voltage: raw volts or a percentage estimate. */
+enum class BatteryReadout { VOLTS, PERCENT }
